3b21cecde721081cce79509a8a16d4ef1aa65819
Leo <leo.m@hotmail.co.uk>
2017-05-09 20:06:17 +0200

    Setup individual @NgModules
    Adding the Firebase Database and Auth Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 
 import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from '../environments/environment';
 
 @NgModule({
@@ -17,6 +19,8 @@ import { environment } from '../environments/environment';
     FormsModule,
     HttpModule,
     AngularFireModule.initializeApp(environment.firebase, 'new-app'), // imports firebase/app needed for everything
+    AngularFireDatabaseModule, // imports firebase/database, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features
   ],
   providers: [],
   bootstrap: [AppComponent]
