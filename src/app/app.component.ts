10ffefe025a7f0b3f00aadb3abe94486bbb648ac
Leo <leo.m@hotmail.co.uk>
2017-05-10 19:24:18 +0200

    Create a query with observable values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-root',
@@ -7,28 +8,19 @@ import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable }
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  items: FirebaseListObservable<any>;
+  items: FirebaseListObservable<any[]>;
+  sizeSubject: Subject<any>;
+  
   constructor(db: AngularFireDatabase) {
-    this.items = db.list('/items');
-    const queryObservable = db.list('/items', {
+    this.sizeSubject = new Subject();
+    this.items = db.list('/items', {
       query: {
-        orderByChild: 'text'
+        orderByChild: 'size',
+        equalTo: this.sizeSubject
       }
     });
-    queryObservable.subscribe(queriedItems => {
-      console.log(queriedItems);
-    })
   }
-  addItem(newName: string) {
-    this.items.push({ text: newName });
-  }
-  updateItem(key: string, newText: string) {
-    this.items.update(key, { text: newText });
-  }
-  deleteItem(key: string) {    
-    this.items.remove(key); 
-  }
-  deleteEverything() {
-    this.items.remove();
+  filterBy(size: string) {
+    this.sizeSubject.next(size);
   }
 }
